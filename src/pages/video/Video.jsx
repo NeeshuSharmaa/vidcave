c3376ddaac6351e9a5c39b924e401ff3d639de5e
Neeshu Sharma <114111237+NeeshuSharmaa@users.noreply.github.com>
2023-07-29 19:45:11 +0530

    built: video page

--- src/pages/video/Video.jsx
+++ src/pages/video/Video.jsx
@@ -1,3 +1,25 @@
+import ReactPlayer from "react-player";
+import { useParams } from "react-router";
+import { useDataContext } from "../../context/DataProvider";
+
 export default function Video() {
-  return <div>Video</div>;
+  const { id } = useParams();
+  const {
+    state: { allVideos },
+  } = useDataContext();
+
+  const video = allVideos?.find(({ _id }) => _id === Number(id));
+
+  return (
+    <div className="column-page-flex">
+      <h2>{video?.title}</h2>
+      <ReactPlayer
+        url={video?.src}
+        controls
+        className="video-container"
+        width="100%"
+        height="500px"
+      />
+    </div>
+  );
 }
