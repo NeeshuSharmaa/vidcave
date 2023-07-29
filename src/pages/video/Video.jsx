9bc67e801590b93b2df47a6d5c92d188e998a720
Neeshu Sharma <114111237+NeeshuSharmaa@users.noreply.github.com>
2023-07-29 20:22:30 +0530

    style: icons on video page

--- src/pages/video/Video.jsx
+++ src/pages/video/Video.jsx
@@ -1,18 +1,23 @@
 import ReactPlayer from "react-player";
 import { useParams } from "react-router";
 import { useDataContext } from "../../context/DataProvider";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock, faClockFour } from "@fortawesome/free-regular-svg-icons";
+import { faBars, faPlus } from "@fortawesome/free-solid-svg-icons";
+import "./Video.css";
 export default function Video() {
   const { id } = useParams();
   const {
-    state: { allVideos },
+    state: { allVideos, watchLater },
+    dispatch,
+    setShowPlaylistModal,
   } = useDataContext();
 
   const video = allVideos?.find(({ _id }) => _id === Number(id));
+  const inWatchLater = watchLater.find((ID) => ID === video._id);
 
   return (
     <div className="column-page-flex">
-      <h2>{video?.title}</h2>
       <ReactPlayer
         url={video?.src}
         controls
@@ -20,6 +25,36 @@ export default function Video() {
         width="100%"
         height="500px"
       />
+      <div className="video-main">
+        <div className="video-info">
+          <img src="/gabby.jpg" alt="creator" />
+          <h3>{video?.title}</h3>
+        </div>
+        <div className="video-actions">
+          <FontAwesomeIcon
+            icon={inWatchLater ? faClockFour : faClock}
+            className={inWatchLater ? "clock blue" : "clock "}
+            onClick={() =>
+              inWatchLater
+                ? dispatch({
+                    type: "REMOVE_FROM_WATCH_LATER",
+                    payload: video._id,
+                  })
+                : dispatch({ type: "ADD_TO_WATCH_LATER", payload: video._id })
+            }
+          />
+          <div
+            className="playlist-icon"
+            onClick={() => setShowPlaylistModal(true)}
+          >
+            <FontAwesomeIcon icon={faBars} className="fa-icon" />
+            <FontAwesomeIcon icon={faPlus} className="fa-icon" />
+          </div>
+        </div>
+      </div>
+      <div className="video-notes">
+        <h2>Video Notes</h2>
+      </div>
     </div>
   );
 }
